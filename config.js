f4b37583a76d80cae54ba78170ada7c999370285
batman-br <jocelitomavila@gmail.com>
2026-03-19 08:57:50 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,25 +1,22 @@
-/* config.js - Galpão Largo (12x3) */
+/* config.js */
 const CONFIG = {
     LAMPADAS_VERTICAL: 7,    
     LAMPADAS_HORIZONTAL: 3,  
-    
     ESPACO_LAMPADA: 36,      
-
-    // 12 eixos verticais (muitas colunas)
-    // 3 eixos horizontais (poucas linhas)
     NUM_EIXOS_V: 10,          
     NUM_EIXOS_H: 4,         
     MARGEM: 60,
 
     VELOCIDADE_OPERADOR: 2,  
-    RAIO_DETECCAO: 30,
+    RAIO_DETECCAO: 40, 
 
-    // Iluminação
-    LIGADAS_AFRENTE: 4,
-    TAXA_FADE: 0.015
+    // Novos Parâmetros
+    MODO_INICIAL: 'REATIVO', // 'REATIVO' ou 'PREDITIVO'
+    TEMPO_LIGADA_MS: 8000,   // 8 segundos ligada após detecção
+    TAXA_FADE: 0.02,         // Velocidade que apaga após o tempo expirar
+    LIGADAS_AFRENTE: 5       // Quantidade de lâmpadas projetadas no Preditivo
 };
 
-// O cálculo automático garante que as prateleiras se ajustem
 CONFIG.CORREDOR_W = CONFIG.ESPACO_LAMPADA; 
 CONFIG.CORREDOR_H = CONFIG.ESPACO_LAMPADA;
 CONFIG.LARGURA_PRATELEIRA = CONFIG.ESPACO_LAMPADA * (CONFIG.LAMPADAS_HORIZONTAL + 1) - CONFIG.CORREDOR_W;
